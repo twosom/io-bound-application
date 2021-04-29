92e69cb46e23e8d35a6018340beb6552dbd9cfa6
desirel <two_somang@icloud.com>
2021-04-29 23:06:23 +0900

    feat: 글 번호로 조회 기능 추가

--- src/main/java/com/icloud/ioboundapplication/PostDto.java
+++ src/main/java/com/icloud/ioboundapplication/PostDto.java
@@ -1,8 +1,12 @@
 package com.icloud.ioboundapplication;
 
+import lombok.AllArgsConstructor;
 import lombok.Data;
+import lombok.NoArgsConstructor;
 
 @Data
+@AllArgsConstructor
+@NoArgsConstructor
 public class PostDto {
     private Long id;
     private String content;
