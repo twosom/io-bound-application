d5a2989b3744fe53dac6e7cd6f978fb2fd7bd273
desirel <two_somang@icloud.com>
2021-04-30 02:38:45 +0900

    feat: 모든 데이터 일괄 삭제로 수정

--- src/main/java/com/icloud/ioboundapplication/PostController.java
+++ src/main/java/com/icloud/ioboundapplication/PostController.java
@@ -74,7 +74,7 @@ public class PostController {
     /* 5. 적합한 테스트를 위해 모든 데이터 제거 */
     @DeleteMapping("/delete_all")
     public ResponseEntity<String> deleteAll() {
-        postRepository.deleteAll();
+        postRepository.deleteAllInBatch();
 
         return ResponseEntity
                 .status(HttpStatus.OK)
