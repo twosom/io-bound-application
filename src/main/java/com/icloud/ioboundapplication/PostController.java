70a935f95231071d081336bc12ef95987b7bbeb8
desirel <two_somang@icloud.com>
2021-04-29 22:51:52 +0900

    feat: paging 적용

--- src/main/java/com/icloud/ioboundapplication/PostController.java
+++ src/main/java/com/icloud/ioboundapplication/PostController.java
@@ -2,13 +2,15 @@ package com.icloud.ioboundapplication;
 
 import lombok.RequiredArgsConstructor;
 import org.modelmapper.ModelMapper;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.PageImpl;
+import org.springframework.data.domain.PageRequest;
+import org.springframework.data.domain.Sort;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
+import java.awt.print.Pageable;
 import java.util.List;
 import java.util.stream.Collectors;
 
@@ -18,6 +20,8 @@ public class PostController {
 
     private final PostRepository postRepository;
     private final ModelMapper mapper;
+
+    private static final Integer PAGE_SIZE = 20;
 
     /* 1. 글을 작성한다. */
     @PostMapping("/post")
@@ -29,19 +33,23 @@ public class PostController {
                 .body(result);
     }
 
-    /* 2-1. 글 목록을 조회한다. */
+    /* 2. 글 목록을 페이징하여 반환 */
     @GetMapping("/posts")
-    public ResponseEntity<List<PostDto>> getPostList() {
-        List<PostDto> collect = postRepository.findAll()
+    public ResponseEntity<Page<PostDto>> getPostList(@RequestParam(defaultValue = "1") Integer page) {
+        Page<Post> result = postRepository.findAll(
+                PageRequest.of(page - 1, PAGE_SIZE, Sort.by("id").descending())
+        );
+
+        List<PostDto> collect = result.getContent()
                 .stream().map(post -> mapper.map(post, PostDto.class))
                 .collect(Collectors.toList());
 
         return ResponseEntity
                 .status(HttpStatus.OK)
-                .body(collect);
+                .body(new PageImpl<>(collect));
     }
 
-    /* 2-2. 글 목록을 페이징하여 반환 */
+
 
     /* 3. 글 번호로 조회 */
 
