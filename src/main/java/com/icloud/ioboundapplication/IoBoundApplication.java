7af94ea2143974fa83821103d4fe9f026b05d782
desirel <two_somang@icloud.com>
2021-04-30 03:54:15 +0900

    feat: 첫 페이지 캐싱

--- src/main/java/com/icloud/ioboundapplication/IoBoundApplication.java
+++ src/main/java/com/icloud/ioboundapplication/IoBoundApplication.java
@@ -5,8 +5,10 @@ import org.modelmapper.convention.MatchingStrategies;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.Bean;
+import org.springframework.scheduling.annotation.EnableScheduling;
 
 @SpringBootApplication
+@EnableScheduling
 public class IoBoundApplication {
 
     public static void main(String[] args) {
