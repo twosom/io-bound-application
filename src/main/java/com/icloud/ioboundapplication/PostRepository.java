815236873747b94dc921eba8ba41b2281dd9fe8b
desirel <two_somang@icloud.com>
2021-04-29 23:28:51 +0900

    feat: 글 내용으로 검색하는 기능 추가

--- src/main/java/com/icloud/ioboundapplication/PostRepository.java
+++ src/main/java/com/icloud/ioboundapplication/PostRepository.java
@@ -2,6 +2,9 @@ package com.icloud.ioboundapplication;
 
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.List;
+
 public interface PostRepository extends JpaRepository<Post, Long> {
 
+    List<Post> findByContentContains(String content);
 }
