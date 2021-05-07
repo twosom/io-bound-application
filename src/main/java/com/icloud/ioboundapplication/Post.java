9a53c17d9e23e921cdc174a10b8413e527033103
desirel <two_somang@icloud.com>
2021-05-07 16:12:31 +0900

    feat: SpringDataJPA -> ElasticSearch

--- src/main/java/com/icloud/ioboundapplication/Post.java
+++ src/main/java/com/icloud/ioboundapplication/Post.java
@@ -3,16 +3,16 @@ package com.icloud.ioboundapplication;
 import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
-
-import javax.persistence.*;
+import org.springframework.data.annotation.Id;
+import org.springframework.data.elasticsearch.annotations.Document;
 
 @Data
-@Entity
+@Document(indexName = "post")
 @NoArgsConstructor
 @AllArgsConstructor
 public class Post {
-    @Id @GeneratedValue(strategy = GenerationType.SEQUENCE, generator = "post_id_seq")
-    @SequenceGenerator(name = "post_id_seq", sequenceName = "post_id_seq", allocationSize = 1)
-    private Long id;
+    @Id
+    private String id;
+
     private String content;
 }
--- src/main/java/com/icloud/ioboundapplication/PostRepository.java
+++ src/main/java/com/icloud/ioboundapplication/PostRepository.java
@@ -1,9 +1,9 @@
 package com.icloud.ioboundapplication;
 
-import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.elasticsearch.repository.ElasticsearchRepository;
 import java.util.List;
 
-public interface PostRepository extends JpaRepository<Post, Long> {
+public interface PostRepository extends ElasticsearchRepository<Post, String> {
 
-    List<Post> findByContentContains(String content);
+    List<Post> findByContent(String content);
 }
